0765c45c02955ff8f8eb2a49d70460b5e9e3cce8
nguyenatu188 <nnguyentu13@gmail.com>
2024-10-02 20:23:28 +0700

    starss

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,6 @@ import * as random from 'maath/random/dist/maath-random.esm'
 const Stars = (props) => {
   const ref = useRef()
   const sphere = random.inSphere(new Float32Array(6000), { radius: 1.2 })
-  console.log(sphere)
   
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10
