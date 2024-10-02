7cb491b52d15b78c4a16ddfe1ec022f3dc6b7ea0
nguyenatu188 <nnguyentu13@gmail.com>
2024-10-02 20:21:52 +0700

    fix stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,9 +5,8 @@ import * as random from 'maath/random/dist/maath-random.esm'
 
 const Stars = (props) => {
   const ref = useRef()
-  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  const sphere = random.inSphere(new Float32Array(6000), { radius: 1.2 })
   console.log(sphere)
-
   
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10
