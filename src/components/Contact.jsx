cf6d726f57405851f185652cff4fd795befc9caf
nguyenatu188 <nnguyentu13@gmail.com>
2024-10-02 13:28:15 +0700

    bla

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -71,6 +71,7 @@ const Contact = () => {
         >
           <p className={styles.sectionSubText}>Get in touch</p>
           <h3 className={styles.sectionHeadText}>Contact.</h3>
+          
           <form
             ref={formRef}
             onSubmit={handleSubmit}
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -75,11 +75,8 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described
-          with links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          Following projects showcasing my skills and experience through my previous works. Each project is briefly described
+          with links to it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively.
         </motion.p>
       </div>
 
